0ab532567e15fa1daaea5ef1ec6cf49a53f07f5a
Marek Barszcz <marek.barszcz@fortedigital.no>
2020-11-15 11:19:42 +0100

    Added

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 (() => {
   const hackathonDate = new Date("December 17, 2020 00:00:00");
   const countdown = document.querySelector(".landing__countdown");
+  let dateString = "";
 
   const updateTime = () => {
     const formatInt = (i) =>
       i.toLocaleString("pl-PL", { minimumIntegerDigits: 2 });
     let diffInSeconds = Math.abs(hackathonDate - Date.now()) / 1000;
-    let secondsLeft = parseInt(diffInSeconds % 60);
-    let minutesLeft = parseInt((diffInSeconds / 60) % 60);
-    let hoursLeft = parseInt((diffInSeconds / (60 * 60)) % 24);
-    let daysLeft = parseInt(diffInSeconds / (60 * 60 * 24));
 
-    let dateString = `${formatInt(daysLeft)}:${formatInt(
-      hoursLeft
-    )}:${formatInt(minutesLeft)}:${formatInt(secondsLeft)}`;
+    if(diffInSeconds > 0){
+      let secondsLeft = parseInt(diffInSeconds % 60);
+      let minutesLeft = parseInt((diffInSeconds / 60) % 60);
+      let hoursLeft = parseInt((diffInSeconds / (60 * 60)) % 24);
+      let daysLeft = parseInt(diffInSeconds / (60 * 60 * 24));
+
+      dateString = `${formatInt(daysLeft)}:${formatInt(
+        hoursLeft
+      )}:${formatInt(minutesLeft)}:${formatInt(secondsLeft)}`;
+    }
+    else{
+      dateString = "OTWARTE";
+    }
+
     countdown.setAttribute("time-left", dateString);
     countdown.textContent = dateString;
   };
@@ -24,3 +32,4 @@
 
   updateTime();
 })();
+
